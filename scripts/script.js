0aeff7d0d1e82e210f8534b695be1c5dc3333962
annafractuous <anna.rasshivkina@flatironschool.com>
2016-09-06 16:15:46 -0400

    adding a quantitative scale

--- scripts/script.js
+++ scripts/script.js
@@ -16,12 +16,29 @@ var writers = [
       color : '#259286' }
 ]
 
-d3.select('#chart')
-    .selectAll('div')
-    .data(writers)
-    .enter().append('div')
-        .classed('item', true)
-        .text(function(d) { return d.name + " | " + d.booksCount })
-        .style('width', function(d) { return d.booksCount * 30 + "px" })
-        .style('background', function(d) { return d.color })
-        .style('color', 'white')
+var barData = [13,29,98,63,77];
+
+var height = 400,
+    width = 600,
+    barWidth = 50,
+    barOffset = 5;
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(barData)])
+    .range([0, height - 20]);
+
+d3.select('#chart').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('background', 'lightblue')
+    .selectAll('rect').data(barData)
+    .enter().append('rect')
+        .style('fill', '#C61C6F')
+        .attr('width', barWidth)
+        .attr('height', function(d) { return yScale(d); })
+        .attr('x', function(d, i) {
+            return i * (barWidth + barOffset);
+        })
+        .attr('y', function(d) {
+            return height - yScale(d);
+        });
