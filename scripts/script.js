1e74aaf588f9dca9401cd91d9ce7be08b22bd60e
annafractuous <anna.rasshivkina@flatironschool.com>
2016-09-07 15:34:32 -0400

    add tooltip

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 var barData = [];
 for (var i = 0; i < 100; i++) {
-  barData.push(Math.random() * 10);
+  barData.push(Math.round(Math.random() * 50));
 }
 
 var height = 400,
@@ -10,8 +10,6 @@ var colors = d3.scaleLinear()
     .domain([0, barData.length*.33, barData.length*.66, barData.length])
     .range(['red', 'purple', 'blue', 'orange'])
 
-var currentColor;
-
 var yScale = d3.scaleLinear()
     .domain([0, d3.max(barData)])
     .range([0, height - 20]);
@@ -20,6 +18,12 @@ var xScale = d3.scaleBand()
     .domain(d3.range(0, barData.length))
     .rangeRound([0, width])
     .paddingInner(0.05)
+
+var tooltip = d3.select('body').append('div')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0);
 
 var chart = d3.select('#chart').append('svg')
     .attr('width', width)
@@ -35,28 +39,33 @@ var chart = d3.select('#chart').append('svg')
         })
         .attr('height', 0)
         .attr('y', height)
-        .transition().duration(700)
-            .attr('height', function(d) {
-                return yScale(d);
-            })
-            .attr('y', function(d) {
-                return height - yScale(d);
-            })
-            .delay(function(d, i) {
-                return i * 10;
-            })
 
-chart
-  .on('mouseover', function(d) {
-    currentColor = this.style.fill;
+chart.transition().duration(700)
+      .attr('height', function(d) {
+          return yScale(d);
+      })
+      .attr('y', function(d) {
+          return height - yScale(d);
+      })
+      .delay(function(d, i) {
+          return i * 10;
+      });
+
+chart.on('mouseover', function(d) {
     d3.select(this)
       .style('opacity', .5)
-      .style('fill', 'lightblue')
       .style('stroke', 'darkblue');
+
+    tooltip.transition()
+      .style('opacity', .9);
+
+    tooltip.html(d)
+      .style('top', (d3.event.pageY - 30) + 'px')
+      .style('left', d3.event.pageX + 'px');
+
   })
   .on('mouseout', function(d) {
     d3.select(this)
       .style('opacity', 1)
-      .style('fill', currentColor)
       .style('stroke', 'none');
   });
